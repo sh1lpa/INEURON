09ff210e3621989bb42e44ffe592237224b8b363
sh1lpa <shilpashinde25@gmail.com>
2023-04-27 15:48:08 +0530

    assignment 5

--- PythonBasics/demo.py
+++ PythonBasics/demo.py
@@ -12,13 +12,19 @@
 
 # if __name__ == '__main__':
 #     main()
-import copy
-l1 = [[1,2,3],4,5,6,7,8]
-l_s = copy.copy(l1)
-l_d = copy.deepcopy(l1)
+# import copy
+# l1 = [[1,2,3],4,5,6,7,8]
+# l_s = copy.copy(l1)
+# l_d = copy.deepcopy(l1)
 
-l1[0][0] = 'a'
+# l1[0][0] = 'a'
 
-print(l1)
-print(l_s)
-print(l_d)
+# print(l1)
+# print(l_s)
+# print(l_d)
+
+import json
+import pprint
+d = {'foo':42, 'bar':32,'l':[1,2,3,44,4,4]}
+print(json.dumps(d))
+pprint.pprint(d)
