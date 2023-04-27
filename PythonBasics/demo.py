a9fc026e2a08955dad066da1c4ee63f15b785282
sh1lpa <shilpashinde25@gmail.com>
2023-04-27 14:50:33 +0530

    assignment 4

--- PythonBasics/demo.py
+++ PythonBasics/demo.py
@@ -1,15 +1,24 @@
 
-x = 10
-def show():
-    global x 
-    print(x)
-    x = x+1
+# x = 10
+# def show():
+#     global x 
+#     print(x)
+#     x = x+1
 
-def main():
-    print(x)
-    print(show())
-    print(x)
+# def main():
+#     print(x)
+#     print(show())
+#     print(x)
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+import copy
+l1 = [[1,2,3],4,5,6,7,8]
+l_s = copy.copy(l1)
+l_d = copy.deepcopy(l1)
 
+l1[0][0] = 'a'
+
+print(l1)
+print(l_s)
+print(l_d)
